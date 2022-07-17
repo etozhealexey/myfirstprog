b46dbefe9bac70b1d538295de71e94291b5913e6
Alexey <tolstunov.alexey@gmail.com>
2022-07-17 23:06:41 +0300

    prog.py

---
+++ prog.py
@@ -0,0 +1,25 @@
+def prog():
+    a = int(input('Введите число '))
+    work = True
+    while work:
+        if a < 10:
+            print('Число меньше или равно 10')
+            if input("Again or stop? ") == 'stop':
+                work = False
+            else:
+                prog()
+
+
+        elif a == 10:
+            print('число  равно 10')
+            if input("Again or stop? ") == 'stop':
+                work = False
+            else:
+                prog()
+
+        else:
+            print("Введите число в диапозоне от 1 до 10")
+            prog()
+
+
+prog()
